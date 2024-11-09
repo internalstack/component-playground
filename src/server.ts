08884c2df236949d31c051246529d8aae81490d9
Alex Fong <alex@orderdive.com>
2024-11-08 22:29:27 -0600

    feat: add heading and paragraph samples

--- src/server.ts
+++ src/server.ts
@@ -34,6 +34,16 @@ server.statefulSession(async(io, { user, sessionId }) => {
     if (componentChoice === 'checkbox') {
       await io.input.checkbox('Initiate self-destruct sequence?')
     }
+    if (componentChoice === 'paragraph') {
+      const { destroy } = await io.display.paragraph('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+      await new Promise(r => setTimeout(r, 5000))
+      destroy()
+    }
+    if (componentChoice === 'heading') {
+      const { destroy } = await io.display.heading('This is a heading!')
+      await new Promise(r => setTimeout(r, 5000))
+      destroy()
+    }
     if (componentChoice === 'checkboxes') {
       await io.input.checkboxes('Pick colors', [
         { label: 'Red', value: 'red' },
